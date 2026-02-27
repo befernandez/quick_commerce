c3c587e899189514c980109ded00705182aad18c
befernandez <98980926+befernandez@users.noreply.github.com>
2026-02-27 10:02:16 -0300

    Update SQL query to use new data source

--- analysis/last_aisle/FS_2026_02.sql
+++ analysis/last_aisle/FS_2026_02.sql
@@ -6,7 +6,7 @@ WITH base AS
     product_score,
     CONCAT(CAST(remote_product_id AS STRING), ';', CAST(product_score AS STRING)) AS product_id_score
   FROM  
-    `peya-food-and-groceries.automated_tables_reports.prueba_last_aisle` , UNNEST(top_50_products) AS p
+    `peya-bi-tools-pro.il_qcommerce.last_aisle_scoring_by_partner`  , UNNEST(top_50_products) AS p
   WHERE
     TRUE
     AND SAFE_CAST(remote_vendor_id AS INT64) IS NOT NULL
