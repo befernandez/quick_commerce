a42d72617e8c7761fb304e84e40afae3594b8ff7
befernandez <98980926+befernandez@users.noreply.github.com>
2026-04-01 10:20:36 -0300

    Enhance SQL with events and transactions CTEs
    Added new CTEs for events and transactions, and updated order attribution logic.

--- tablas/fact_qc_orders.sql
+++ tablas/fact_qc_orders.sql
@@ -22,6 +22,7 @@ WITH sessionid AS
     ROW_NUMBER() OVER(PARTITION BY o.order_id ORDER BY session_start_timestamp_utc ASC) = 1
 ) 
 
+
 , double_ya AS 
 (
   SELECT DISTINCT 
@@ -161,7 +162,121 @@ WITH sessionid AS
     GROUP BY 1
  )
  
+, events AS 
+(
+  SELECT
+    e.platform,
+    e.userId,
+    e.partition_date,
+    e.payload_timestamp_local,
+    CONCAT(e.sessionId, e.global_entity_id) as session_id,
+    e.eventAction,
+    e.eventVariablesJson
+  FROM 
+    `peya-data-origins-pro.cl_sessions.perseus_events` e
+  WHERE 
+    TRUE
+    AND e.partition_date >= start_date
+    AND DATE(e.payload_timestamp_local) >= start_date
+    AND e.platform in ("Android","iOS")
+    AND CONCAT(e.sessionId, e.global_entity_id) IS NOT NULL 
+    AND 
+    ( 
+      (e.eventAction IN ('transaction') AND (LOWER(STRING(eventVariablesJson.checkoutType)) IN ("groceries")))
+      OR (e.eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.businessType)) NOT IN ("restaurant"))
+      OR (e.eventAction IN ('shop_details.loaded') 
+        AND LOWER(STRING(eventVariablesJson.businessType)) NOT IN ("restaurant") 
+        AND COALESCE(LOWER(STRING(eventVariablesJson.eventOrigin)),LOWER(STRING(eventVariablesJson.origin)))  IN ("entry-point-service", "custom-entry-points-service"))
+    )
+)
+
+
+-- Identificamos las transacciones
+, transactions AS 
+(
+  SELECT * 
+  FROM 
+  (
+    SELECT
+    session_id
+    , CASE WHEN STRING(eventVariablesJson.sequence) IS NULL THEN 'single_order'
+         WHEN STRING(eventVariablesJson.sequence) = "2" THEN 'double_ya'
+         ELSE NULL END AS order_type
+    , STRING(eventVariablesJson.transactionId) as transactionId
+    , CASE WHEN STRING(eventVariablesJson.sequence) IS NULL THEN STRING(eventVariablesJson.businessType)
+         WHEN STRING(eventVariablesJson.sequence) = "2" THEN STRING(eventVariablesJson.businessTypeSecondary)
+         ELSE NULL END AS businessType
+    , CASE WHEN STRING(eventVariablesJson.sequence) IS NULL THEN STRING(eventVariablesJson.shopId)
+         WHEN STRING(eventVariablesJson.sequence) = "2" THEN STRING(eventVariablesJson.shopIdSecondary)
+         ELSE NULL END AS shopId
+     , ROW_NUMBER() OVER (PARTITION BY STRING(eventVariablesJson.transactionId) ORDER BY payload_timestamp_local ASC) as row_num
+    FROM 
+      events
+    WHERE 
+      TRUE
+      AND eventAction IN ('transaction') --AND (STRING(eventVariablesJson.sequence) IS NULL OR STRING(eventVariablesJson.sequence) = "2")
+  ) 
+  WHERE 
+    TRUE
+    AND row_num = 1
+)
+-- identificamos el primer shop.clicked
+, shop_clicked AS 
+(
+  SELECT DISTINCT 
+    session_id
+    , STRING(eventVariablesJson.shopId) AS shopId
+    , STRING(eventVariablesJson.businessType) AS businessType
+    , STRING(eventVariablesJson.shopCategorySelected) AS categorySelected
+    , CASE WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('home_screen') THEN "Home"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('campaign_landing', 'incentives_voucher_landing', 'incentives_pm_landing') THEN "Home"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('verticalhome') THEN "QC Shop List"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('homesearchdetails') THEN "Home Presearch"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('verticalsearchdetails') THEN "QC Presearch"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('searchresults') AND REGEXP_CONTAINS(LOWER(STRING(eventVariablesJson.shopSponsoringPlacement)), 'home') THEN "Home Partner Search"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('searchresults') AND NOT REGEXP_CONTAINS(LOWER(STRING(eventVariablesJson.shopSponsoringPlacement)), 'home') THEN "QC Partner Search"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('nestedsearchresults') AND REGEXP_CONTAINS(LOWER(STRING(eventVariablesJson.shopSponsoringPlacement)), 'home') THEN "Home Product Search"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('nestedsearchresults') AND NOT REGEXP_CONTAINS(LOWER(STRING(eventVariablesJson.shopSponsoringPlacement)), 'home') THEN "QC Product Search"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('nullverticalsearchresults') AND REGEXP_CONTAINS(LOWER(STRING(eventVariablesJson.shopSponsoringPlacement)), 'home') THEN "Home Null Search"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('nullverticalsearchresults') AND NOT REGEXP_CONTAINS(LOWER(STRING(eventVariablesJson.shopSponsoringPlacement)), 'home') THEN "QC Null Search" 
+          WHEN eventAction IN ('shop.clicked') AND REGEXP_CONTAINS(LOWER(STRING(eventVariablesJson.screenName)), 'otp') THEN "OTP"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('promodetails') THEN "QC Promo Details"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('promotions-section-home') THEN "Promo Section"
+          WHEN eventAction IN ('shop.clicked') AND LOWER(STRING(eventVariablesJson.screenName)) IN ('promotions-section-qc') THEN "Promo Section QC"
+          WHEN eventAction IN ('shop.clicked') AND REGEXP_CONTAINS(LOWER(STRING(eventVariablesJson.screenName)), 'cart') THEN "Cart"
+          WHEN eventAction IN ('shop_details.loaded') THEN "Home Entry Point"
+          ELSE NULL 
+      END AS order_attribution
+  FROM 
+    events
+  WHERE 
+    eventAction IN ('shop.clicked', 'shop_details.loaded')
+  QUALIFY ROW_NUMBER() OVER(PARTITION BY session_id, STRING(eventVariablesJson.shopId) ORDER BY payload_timestamp_local ASC) = 1
+) 
+, order_att AS 
+(
+  SELECT 
+    t.session_id,
+    t.order_type,
+    t.transactionId as transaction_id,
+    t.shopId as shop_id,
+    t.businessType as business_type,
+    sc.categorySelected as category_selected,
+    CASE 
+      WHEN t.order_type IN ('single_order') THEN sc.order_attribution
+      WHEN t.order_type IN ('double_ya') THEN "DoubleYa" 
+      ELSE NULL 
+    END AS order_attribution,
+  FROM
+    transactions t
+  LEFT JOIN 
+    shop_clicked sc
+    ON t.session_id = sc.session_id 
+    AND t.shopId = sc.shopId
+)
+
  
+
 SELECT DISTINCT 
    o.registered_date,
    o.order_id,
@@ -186,37 +301,38 @@ SELECT DISTINCT
     WHEN o.business_type.business_type_name in ('Market','Kiosks','Drinks','Pets','Pharmacy','Shop') AND (is_aaa is true or  REGEXP_CONTAINS(LOWER(partner_name), r"pedidosya|pya")) then 1 
     ELSE 0 
    END AS groceries_order,
-   mission_type,
-   o.is_pre_order,
-   o.order_status,  
-   CASE WHEN dy.order_id IS NOT NULL THEN 1 ELSE 0 END AS is_double_ya,
-   o.promisedDeliveryTime.minMinutes AS min_pdt,
-   o.promisedDeliveryTime.maxMinutes AS max_pdt, 
-   ns.actual_delivery_time,
-   CASE WHEN ns.is_non_seamless_order = 1 THEN 1 ELSE 0 END AS is_non_seamless_order,
-   CASE WHEN ns.is_slow_order = 1 THEN 1 ELSE 0 END AS is_slow_order,
-   CASE WHEN ns.is_late_order = 1 THEN 1 ELSE 0 END AS is_late_order,
-   CASE WHEN ns.is_session_order = 1 THEN 1 ELSE 0 END AS is_session_order,
-   CASE WHEN ns.is_modified_order = 1 THEN 1 ELSE 0 END AS is_modified_order,
-   CASE WHEN has_compensation = 1 THEN 1 ELSE 0 END AS has_compensation,
-   CASE WHEN has_refund = 1 THEN 1 ELSE 0 END AS has_refund,
-   ROUND(fop.gpo,3) AS gpo,
-   ROUND(fop.gpo/rate_eu,3) as gpo_eu,
-   ROUND(p.total_gmv,3) AS total_gmv,
-   ROUND(p.total_gmv/rate_eu,3) AS total_gmv_eu,
-   ROUND(CASE WHEN p.gmv_products_with_promo IS NOT NULL THEN p.gmv_products_with_promo ELSE 0 END,3) AS gmv_products_with_promo,
-   ROUND(CASE WHEN p.gmv_products_with_promo IS NOT NULL THEN p.gmv_products_with_promo/rate_eu ELSE 0 END,3) AS gmv_products_with_promo_eu,
-   p.basket_size,
-   p.qty_distinct_products,
-   CASE WHEN p.qty_products_with_promo IS NOT NULL THEN p.qty_products_with_promo ELSE 0 END AS qty_products_with_promo,
-   CASE WHEN p.qty_distinct_products_with_promo IS NOT NULL THEN p.qty_distinct_products_with_promo ELSE 0 END AS qty_distinct_products_with_promo,
-   ROUND(CASE WHEN p.share_products_with_promo IS NOT NULL THEN p.share_products_with_promo ELSE 0 END,3) AS share_products_with_promo,
-   ROUND(CASE WHEN p.share_gmv_products_with_promo IS NOT NULL THEN p.share_gmv_products_with_promo ELSE 0 END,3) AS share_gmv_products_with_promo,
-   p.qty_distinct_cat_level_one,
-   p.has_fresh_products,
-   p.has_ultra_fresh_products,
-   p.has_produce_products,
-   p.has_meat_seafood_products,  
+    mission_type,
+    o.is_pre_order,
+    o.order_status,  
+    CASE WHEN dy.order_id IS NOT NULL THEN 1 ELSE 0 END AS is_double_ya,
+    o.promisedDeliveryTime.minMinutes AS min_pdt,
+    o.promisedDeliveryTime.maxMinutes AS max_pdt, 
+    ns.actual_delivery_time,
+    CASE WHEN ns.is_non_seamless_order = 1 THEN 1 ELSE 0 END AS is_non_seamless_order,
+    CASE WHEN ns.is_slow_order = 1 THEN 1 ELSE 0 END AS is_slow_order,
+    CASE WHEN ns.is_late_order = 1 THEN 1 ELSE 0 END AS is_late_order,
+    CASE WHEN ns.is_session_order = 1 THEN 1 ELSE 0 END AS is_session_order,
+    CASE WHEN ns.is_modified_order = 1 THEN 1 ELSE 0 END AS is_modified_order,
+    CASE WHEN has_compensation = 1 THEN 1 ELSE 0 END AS has_compensation,
+    CASE WHEN has_refund = 1 THEN 1 ELSE 0 END AS has_refund,
+    ROUND(fop.gpo,3) AS gpo,
+    ROUND(fop.gpo/rate_eu,3) as gpo_eu,
+    ROUND(p.total_gmv,3) AS total_gmv,
+    ROUND(p.total_gmv/rate_eu,3) AS total_gmv_eu,
+    ROUND(CASE WHEN p.gmv_products_with_promo IS NOT NULL THEN p.gmv_products_with_promo ELSE 0 END,3) AS gmv_products_with_promo,
+    ROUND(CASE WHEN p.gmv_products_with_promo IS NOT NULL THEN p.gmv_products_with_promo/rate_eu ELSE 0 END,3) AS gmv_products_with_promo_eu,
+    p.basket_size,
+    p.qty_distinct_products,
+    CASE WHEN p.qty_products_with_promo IS NOT NULL THEN p.qty_products_with_promo ELSE 0 END AS qty_products_with_promo,
+    CASE WHEN p.qty_distinct_products_with_promo IS NOT NULL THEN p.qty_distinct_products_with_promo ELSE 0 END AS qty_distinct_products_with_promo,
+    ROUND(CASE WHEN p.share_products_with_promo IS NOT NULL THEN p.share_products_with_promo ELSE 0 END,3) AS share_products_with_promo,
+    ROUND(CASE WHEN p.share_gmv_products_with_promo IS NOT NULL THEN p.share_gmv_products_with_promo ELSE 0 END,3) AS share_gmv_products_with_promo,
+    p.qty_distinct_cat_level_one,
+    p.has_fresh_products,
+    p.has_ultra_fresh_products,
+    p.has_produce_products,
+    p.has_meat_seafood_products,  
+    att.order_attribution
   FROM 
    `peya-bi-tools-pro.il_core.fact_orders` o
   LEFT JOIN 
@@ -247,9 +363,11 @@ SELECT DISTINCT
   LEFT JOIN 
     products AS p 
     ON p.order_id = o.order_id
+  LEFT JOIN 
+    order_att AS att
+    ON SAFE_CAST(att.transaction_id AS INT64)= o.order_id
   WHERE
     TRUE
     AND o.registered_date >= start_date
     AND o.business_type.business_type_name in ('Market','Kiosks','Drinks','Pets','Pharmacy','Shop')
-
  
